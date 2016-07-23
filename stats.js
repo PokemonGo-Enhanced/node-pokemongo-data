cad4a6d2ea7aa3f04220868359f98b5110461c98
Ruben Vereecken <rubenvereecken@gmail.com>
2016-07-23 18:40:04 +0200

    Init

---
+++ stats.js
@@ -0,0 +1,11 @@
+class Stats {
+  powerQuotient(pokemon) {
+    let atk = pokemon.individual_attack || 0;
+    let def = pokemon.individual_defense || 0;
+    let sta = pokemon.individual_stamina || 0;
+    return (atk + def + sta) / 45;
+  },
+}
+
+
+module.exports = new Stats();
