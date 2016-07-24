baec7ad60d6d496f41044a5b486386e62be41607
AVVS <v@aminev.me>
2016-07-24 17:12:05 +0300

    fix 0 values

--- src/stats.js
+++ src/stats.js
@@ -2,6 +2,8 @@ import leveling from '../leveling.json';
 import { byName as PokemonByName } from './data';
 import { byName as moveByName } from './moves';
 
+const { pow, floor, round } = Math;
+
 // Correct spreadsheet
 // https://docs.google.com/spreadsheets/d/1xZ1daunztOgvOZxO7ABfJPF41-2mqMDzY1Qs_0OWwq8/edit#gid=320772897
 // determine cp multiplier by level, each 0.5 levels we get:
@@ -30,10 +32,7 @@ leveling.forEach(level => {
 
 // how perfect pokemon is, IVs
 export const powerQuotient = pokemon => {
-  const atk = pokemon.individual_attack || 0;
-  const def = pokemon.individual_defense || 0;
-  const sta = pokemon.individual_stamina || 0;
-  return Math.round((atk + def + sta) / 45 * 100);
+  return round((atk(pokemon) + def(pokemon) + sta(pokemon)) / 45 * 100);
 };
 
 // current cp, based on when pokemon was caught + how many power ups happened
@@ -44,14 +43,17 @@ export const cpMultiplier = pokemon =>
 export const cp = (multiplier, attack, stamina, defense) =>
   floor(attack * pow(defense, 0.5) * pow(stamina, 0.5) * pow(multiplier, 2) / 10);
 
+export const atk = (pokemon) => pokemon.individual_attack || 0;
+export const def = (pokemon) => pokemon.individual_defense || 0;
+export const sta = (pokemon) => pokemon.individual_stamina || 0;
+
 export const relativeInfluence = (...baseStats) => {
   const sum = baseStats.reduce((stat, value) => stat + value, 0);
-  return Math.round(((sum + 15 * baseStats.length) / sum - 1) * 100) + '%';
+  return round(((sum + 15 * baseStats.length) / sum - 1) * 100) + '%';
 };
 
 // returns pokemons current stats
 // based on pokemon and player's level
-const { pow, floor } = Math;
 export const calc = (pokemon, _level) => {
   if (_level < 1 || _level > 40) {
     throw new Error('level must be >= 1 and <= 40');
@@ -66,9 +68,9 @@ export const calc = (pokemon, _level) => {
   const maxCpMultiplier = cpByLevel[level];
 
   // current stats of the pokemon
-  const attack = (BaseAttack + pokemon.individual_attack);
-  const defense = (BaseDefense + pokemon.individual_defense);
-  const stamina = (BaseStamina + pokemon.individual_stamina);
+  const attack = (BaseAttack + atk(pokemon));
+  const defense = (BaseDefense + def(pokemon));
+  const stamina = (BaseStamina + sta(pokemon));
 
   // max CP for this trainer level
   const maxCombatPower = cp(maxCpMultiplier, attack, stamina, defense);
