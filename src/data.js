e94a3afa56ec9834cab993589dc4ed5bcc98dea6
Ruben Vereecken <rubenvereecken@gmail.com>
2016-07-26 20:32:11 +0200

    More numbers more null

--- src/data.js
+++ src/data.js
@@ -4,18 +4,32 @@ import changeCase from 'change-case';
 export const byName = {};
 export const byNumber = {};
 
-const convertToNumber = ['BaseAttack', 'BaseDefense', 'BaseStamina'];
+const convertToNumber = ['PkMn', 'EvolvesFrom', 'EvolvesTo', 'EvoChainID',
+  'EvoStage', 'BaseAttack', 'BaseDefense', 'BaseStamina', 'BaseCaptureRate',
+  'BaseFleeRate', 'CollisionRadiusM', 'CollisionHeightM', 'CollisionHeadRadiusM',
+  'AttackTimerS', 'PokemonClass', 'PokedexHeightM', 'PokedexWeightKg', 'CandyToEvolve'
+];
+
+const nullable = ['EvolvesFrom', 'EvolvesTo']
 
 pokemonList.forEach(pokemon => {
   const normName = changeCase.constantCase(pokemon.Identifier);
+  let shouldBeNumbers = [
+    'PkMn', 'EvolvesFrom', 'EvolvesTo', 'EvoChainID', 'EvoStage',
+  ]
 
   // make sure we have numbers stored
   convertToNumber.forEach(field => {
     pokemon[field] = parseFloat(pokemon[field]);
   });
 
+  nullable.forEach(field => {
+    if (!pokemon[field])
+      pokemon[field] = null;
+  });
+
   // both number / string
   byNumber[pokemon.PkMn] = pokemon;
   byNumber[+pokemon.PkMn] = pokemon;
   byName[normName] = pokemon;
-});
+});k
